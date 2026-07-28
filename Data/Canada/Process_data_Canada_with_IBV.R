88e0592e04466b4059a69b66989dbff3ab17927c
WakinyanB <124592402+WakinyanB@users.noreply.github.com>
2026-07-28 12:11:51 -0400

    Add files via upload

--- Data/Canada/Process_data_Canada_with_IBV.R
+++ Data/Canada/Process_data_Canada_with_IBV.R
@@ -11,46 +11,54 @@ load("Google_mobility/mobility_mean_CA.RData")
 
 data_ca <- read.csv("Canada/data_canada_resp.csv", header=TRUE)[,-1] %>% subset(province!="QC")
 
-data <- merge.data.frame(x=subset(data_ca, type=="RV"|type=="RV/EV"),
-                         y=subset(data_ca, type=="Flu A"), by=c("date", "province"))
+data <- merge(
+  merge(
+    subset(data_ca, type %in% c("RV", "RV/EV")),
+    subset(data_ca, type=="Flu A"),
+    by = c("date", "province"),
+    all.x = TRUE
+  ),
+  subset(data_ca, type=="Flu B"),
+  by = c("date", "province"),
+  all.x = TRUE
+)
 
-all(data$week.x==data$week.y)
+data <- data[,-which(colnames(data) %in% c("week.x", "week.y", "type.x", "type.y", "type", "tests"))]
 
-colnames(data)[which(colnames(data)=="week.x")] <- "week"
 colnames(data)[which(colnames(data)=="positive.x")] <- "RV_positive"
 colnames(data)[which(colnames(data)=="positive.y")] <- "IAV_positive"
+colnames(data)[which(colnames(data)=="positive")] <- "IBV_positive"
 colnames(data)[which(colnames(data)=="tests.x")] <- "RV_test"
-colnames(data)[which(colnames(data)=="tests.y")] <- "IAV_test"
-
-data <- data[,-c(grep("\\.x", colnames(data)), grep("\\.y", colnames(data)))]
+colnames(data)[which(colnames(data)=="tests.y")] <- "IV_test"
 
 data$date <- data$date %>% ymd
 
 data$RV_test_rolling_avg <- NA
-data$IAV_test_rolling_avg <- NA
+data$IV_test_rolling_avg <- NA
 data$RV_testing_factor <- NA
-data$IAV_testing_factor <- NA
+data$IV_testing_factor <- NA
 
 mean_tests <- data %>%
-  ddply(~province, function(X){return(c("RV"=mean(X$RV_test), "IAV"=mean(X$IAV_test)))})
+  ddply(~province, function(X){return(c("RV"=mean(X$RV_test), "IV"=mean(X$IV_test)))})
 
 for(i in 1:nrow(data)){
   
   X <- subset(data, province == data$province[i] &
-                    date >= data$date[i] %m-% months(6) &
-                    date <= data$date[i] %m+% months(6))
+                date >= data$date[i] %m-% months(6) &
+                date <= data$date[i] %m+% months(6))
   
   data$RV_test_rolling_avg[i] <- mean(X$RV_test)
-  data$IAV_test_rolling_avg[i] <- mean(X$IAV_test)
+  data$IV_test_rolling_avg[i] <- mean(X$IV_test)
   
   data$RV_testing_factor[i] <- mean_tests$RV[match(unique(X$province), mean_tests$province)]/
-                               data$RV_test_rolling_avg[i]
-  data$IAV_testing_factor[i] <- mean_tests$IAV[match(unique(X$province), mean_tests$province)]/
-                                data$IAV_test_rolling_avg[i]
+    data$RV_test_rolling_avg[i]
+  data$IV_testing_factor[i] <- mean_tests$IV[match(unique(X$province), mean_tests$province)]/
+    data$IV_test_rolling_avg[i]
 }
 
 data$RV_scaled_cases <- (data$RV_positive/100)*data$RV_test*data$RV_testing_factor
-data$IAV_scaled_cases <- (data$IAV_positive/100)*data$IAV_test*data$IAV_testing_factor
+data$IAV_scaled_cases <- (data$IAV_positive/100)*data$IV_test*data$IV_testing_factor
+data$IBV_scaled_cases <- (data$IBV_positive/100)*data$IV_test*data$IV_testing_factor
 
 ggplot(data, aes(x=date)) +
   facet_wrap(~province, scales="free_y") +
@@ -59,29 +67,18 @@ ggplot(data, aes(x=date)) +
 
 ggplot(data, aes(x=date)) +
   facet_wrap(~province, scales="free_y") +
-  geom_line(aes(y=(IAV_positive/100)*IAV_test), col="grey") +
+  geom_line(aes(y=(IAV_positive/100)*IV_test), col="grey") +
   geom_line(aes(y=IAV_scaled_cases))
 
-sum_IAV_cases_lag <- function(data, lag=0){
-  return(
-    (1:nrow(data)) %>% sapply(function(i){
-      return(sum(data$IAV_scaled_cases[data$province==data$province[i] &
-                                         data$date>=data$date[i]-7*lag &
-                                         data$date<=data$date[i]]))
-    })
-  )
-}
-
-data$IAV_scaled_cases_lag1 <- sum_IAV_cases_lag(data, lag=1)
-data$IAV_scaled_cases_lag3 <- sum_IAV_cases_lag(data, lag=3)
-data$IAV_scaled_cases_lag5 <- sum_IAV_cases_lag(data, lag=5)
+ggplot(data, aes(x=date)) +
+  facet_wrap(~province, scales="free_y") +
+  geom_line(aes(y=(IBV_positive/100)*IV_test), col="grey") +
+  geom_line(aes(y=IBV_scaled_cases))
 
 ggplot(data, aes(x=date)) +
-  facet_wrap(~province, ncol=3, scales="free_y") +
-  geom_line(aes(y=IAV_scaled_cases)) +
-  geom_line(aes(y=IAV_scaled_cases_lag1), col='red') +
-  geom_line(aes(y=IAV_scaled_cases_lag3), col='darkgreen') +
-  geom_line(aes(y=IAV_scaled_cases_lag5), col='orange')
+  facet_wrap(~province, scales="free_y") +
+  geom_line(aes(y=IAV_scaled_cases), col='red') +
+  geom_line(aes(y=IBV_scaled_cases), col='blue')
 
 data_ca <- data %>% subset(province=="Canada")
 data_at <- data %>% subset(province=="At")
@@ -101,4 +98,5 @@ data_bc$c[match(mobility_mean_bc2$week_ending, data_bc$date)] <- mobility_mean_b
 data_on$c[match(mobility_mean_on2$week_ending, data_on$date)] <- mobility_mean_on2$trend4/100
 data_pr$c[match(mobility_mean_pr2$week_ending, data_pr$date)] <- mobility_mean_pr2$trend4/100
 
-# save(data_ca, data_at, data_bc, data_on, data_pr, file="Canada/data_canada_province.RData")
+# save(data_ca, data_at, data_bc, data_on, data_pr,
+#      file="Canada/data_canada_province_with_IBV.RData")
